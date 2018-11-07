2c779cf0dd90813c689fec8b4240f50d15317fd8
Kevin Giguere <kev.giguere.l@gmail.com>
2018-11-07 18:06:13 -0500

    create empty sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
 var open = require('gulp-open');
+var index = require('gulp-index');
+
+gulp.task('copyLib', function() {
+  return gulp.src(require.resolve('parallelio-dom/dist/parallelio-and-dom.js'))
+    .pipe(gulp.dest('./public/lib'));
+});
+
+gulp.task('copyHtml', function() {
+  return gulp.src('./samples/**/*.html')
+    .pipe(gulp.dest('./public/samples'));
+});
+
+gulp.task('buildIndex', function() {
+  return gulp.src('./public/samples/**/*.html')
+    .pipe(index({relativePath: './public/'}))
+    .pipe(gulp.dest('./public/'));
+});
 
 gulp.task('serve', function(done) {
   var server = gls.static('public');
@@ -8,7 +25,9 @@ gulp.task('serve', function(done) {
   done()
 });
  
-gulp.task('open', gulp.series('serve',function(){
+gulp.task('build', gulp.series('copyLib', 'copyHtml', 'buildIndex'));
+
+gulp.task('open', gulp.series('build', 'serve', function(){
   var options = {
     uri: 'http://localhost:3000/index.html'
   };
